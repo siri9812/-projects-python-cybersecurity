157f2d79ebe4b8a7f6ec40752c0b23f1e4f2b0a7
siri9812 <shirishacheruku577@gmail.com>
2026-03-04 11:55:32 +0530

    passwd.mnger.py

--- passwd.mngr.py
+++ passwd.mngr.py
@@ -8,7 +8,7 @@ def create_account():
     password = getpass.getpass("Enter your desired password: ")
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     password_manager[username] = hashed_password
-    print("Account created successfully!")
+    print("Account created successful!")
 
 
 def login():
@@ -16,13 +16,13 @@ def login():
     password = getpass.getpass("Enter your password: ")
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     if username in password_manager.keys() and password_manager[username] == hashed_password:
-        print("login successfully")
+        print("login successful")
     else:
         print("invalid username or password")
 
 def main():
     while True:
-        choice = input("Enter 1 to create an accont, 2 to login, or 0 to exit: ")
+        choice = input("Enter 1 to create an account, 2 to login, or 0 to exit: ")
         if choice == "1":
             create_account()
         elif choice == "2":
@@ -33,4 +33,5 @@ def main():
             print("Invalid choice.")
 
 if __name__ == "__main__":
+
     main()
